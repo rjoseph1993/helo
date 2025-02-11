3a1e8feeef7090256a780ad17608ccf9fd1e2354
rjoseph1993 <ephelinej@gmail.com>
2025-02-10 23:52:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const PORT = process.env.PORT || 8080
 const server = app.listen(8080, "127.0.0.1")
 
 const io = require('socket.io')(server)
-
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 app.use(express.static(path.join(__dirname, 'public')))
 
 let socketsConected = new Set()
